2922d40f12fb248562125a6bb5fac6c43985a912
demonThread <giully@protonmail.com>
2020-05-29 16:05:58 +0200

    input component

---
+++ ecs/component/InputComponent.java
@@ -0,0 +1,11 @@
+package framework3d.ecs.component;
+
+/*
+Questo è uno dei pochi componenti che sarà a gerarchia. Attraverso questo componente, gestisco l'input di 
+una entità, cioè come deve tradurre i dati in input (esempio i tasti della tastiera, intelligenza artificiale ecc),
+in azioni (esempio, rotazione spostamento, spari, ecc)
+*/
+public class InputComponent implements Component
+{
+
+}
