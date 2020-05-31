1b02eaa2ea00a10f3706fbf9d5939073c148c5e3
demonThread <giully@protonmail.com>
2020-05-31 16:21:41 +0200

    inizio game loop

--- ecs/system/RenderingSystem.java
+++ ecs/system/RenderingSystem.java
@@ -1,18 +1,18 @@
 package framework3d.ecs.system;
 
-import java.awt.Graphics;
+//import java.awt.Graphics;
 
 import java.util.ArrayList;
 
 import framework3d.ecs.entity.RenderableEntity;
+import framework3d.geometry.Triangle;
 
 //PROVARE A PARALLELIZZARE CON PARALLEL STREAM. MOLTO PIù DIFFICILE POICHé BISOGNA RISPETTARE UN CERTO ORDINE
 //QUINDI NON SO SE VALGA LA PENA PROVARE A FARLO.
 public class RenderingSystem 
 {
-    //Se al posto di passare un array di entità passassi direttamente un buffeer con i soli triangoli
-    //già ordinati? 
-    public void render(ArrayList<RenderableEntity> renderableEntities, Graphics g)
+
+    public ArrayList<Triangle> render(ArrayList<RenderableEntity> renderableEntities)
     {   
         for (RenderableEntity e : renderableEntities)
         {
--- handler/GameHandler.java
+++ handler/GameHandler.java
@@ -27,11 +27,113 @@ public class GameHandler
     //******** FINE INTERFACCE ENTITA' ****** */
     
 
-    //********* FINETRA ****** */
+    //********* FINESTRA ****** */
     
     private WindowHandler window;
 
-    //********* FINE FINETRA ****** */
+    //********* FINE FINESTRA ****** */
+
+
+    /********************** COSTRUTTORI ***************************************** */
+    public GameHandler()
+    {
+        init();
+    }
+
+    /********************** FINE COSTRUTTORI ***************************************** */
+
+
+
+    /****************************** METODI DI SETUP *********************************** */
+    
+    private void init()
+    {
+        transformSystem = new TransformSystem();
+        inputSystem = new InputSystem();
+    }
+    
+    /****************************** FINE METODI DI SETUP *********************************** */
+
+
+    //********************************* AGGIUNTA ENTITA' ********************************************** */
+    
+    public void addDynamicEntity(DynamicEntity e)
+    {
+        dynamicEntities.add(e);
+    }
+    
+    //********************************* FINE AGGIUNTA ENTITA' ********************************************** */
+
+
+    /*********************************** RIMOZIONE ENTITA' ********************************************** */
+    
+    public void removeDynamicEntity(DynamicEntity e)
+    {
+        dynamicEntities.remove(e);
+    }
+    
+    /*********************************** FINE RIMOZIONE ENTITA' ********************************************** */
+
+
+    /************************************* GAME LOOP *********************************************************** */
+
+    //Calcolo del tempo, passare il tempo ai vari sistemi.
+    public void gameLoop()
+    {
+        int updatePerSecond = 60;
+		long nanoSecond = 1000000000L;
+		float timePerUpdate = nanoSecond / updatePerSecond;
+		float elapsedTime = 0;
+		long currentTime;
+		long lastTime = System.nanoTime();
+        float nsPerFrame;
+        
+        float secPerFrame;
+
+        while (true)
+		{
+			//IMPOSTARE GAME LOOP OGNI SESSANTESIMO DI SECONDO TRAMITE UN IF.
+			currentTime = System.nanoTime();
+
+			elapsedTime += (currentTime - lastTime) / timePerUpdate;
+			
+			nsPerFrame = currentTime - lastTime;
+			
+			lastTime = currentTime;
+
+			//non chiamare il game loop ogni sessantesimo di secondo, ma ottimizzare calcolando lo stesso il frame
+			//da sistemare.
+			//possibile soluzione, multi thread, con update e rendering in due thread diversi.
+			if (elapsedTime >= 1.0)
+			{
+                // nsPerFrame / 1.0E9 Converto in secondi.
+                
+                secPerFrame = nsPerFrame / 1.0E9f;
+                
+                updateInputComponents(secPerFrame);
+                updateTransformComponents(secPerFrame);
+
+                --elapsedTime;
+			}
+			//da controllare se fare il rendering del frame più velocemente possibile e non vincolato dall'update dello stato
+			//del gioco ogni 1/60, sia una cosa positiva alle performance generali e alla visione.
+		}
+    }
+
+
+    //MODIFICARE I SISTEMI COSì DA PRENDERE IN INPUT NELLA FUNZIONE UPDATE IL TEMPO PASSATO.
+    private void updateInputComponents(float elapsedTime)
+    {
+        inputSystem.update(dynamicEntities);
+    }
+
+
+    private void updateTransformComponents(float elapsedTime)
+    {
+        transformSystem.update(dynamicEntities);
+    }
+    
+    /************************************* FINE GAME LOOP *********************************************************** */
 
 
 }
