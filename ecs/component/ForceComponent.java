5907c3b3837060754722c56c1750aea05c891368
Giully314 <giully@protonmail.com>
2020-07-20 17:07:22 +0200

    Cambio funzione update per renderla più leggibile

--- ecs/component/ForceComponent.java
+++ ecs/component/ForceComponent.java
@@ -8,7 +8,7 @@ public class ForceComponent extends Component
     /*
     Questo valore serve solo per le entità controllate da un'intelligenza artificiale o da un giocatore, così da simulare i limiti
     dei motori di una navicella e soprattutto un aumento graduale della spinta. Gli altri oggetti non faranno uso di questa variabile
-    poiché la forza verrà calcolata come forza di gravità tra corpi.
+    poiché la forza verrà calcolata come forza di gravità tra i vari corpi presenti.
     */
     public int maxForceValue = 1000; 
     
